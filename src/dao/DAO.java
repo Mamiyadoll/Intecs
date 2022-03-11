cfbc678afaade8745b67b6a949f5586ba2aa8d5b
nana <1>
2022-03-11 14:33:30 +0900

    メソッド追加
    Signed-off-by: nana <1>

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -8,45 +8,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import beans.CartBean;
 import beans.ProductBean;
 import beans.UserBean;
 
 public class DAO {
-
-//	メソッド型！
-//	public static 戻り値型 メソッド名(引数) {
-//		Connection con = null;
-//		PreparedStatement ps = null;
-//		ResultSet rs = null;
-//		戻り値宣言
-//
-//		try {
-//			データ取得
-//			con =  accessDB();
-//			String sql = "SQL文";
-//			ps = con.prepareStatement(sql);
-//			ps.setString(1, 変数名);
-//			rs = ps.executeQuery();
-//
-//			取得データ格納変数宣言
-//
-//			while(rs.next()) {
-//				rs.getString();
-//			}
-//			インスタンス生成
-//
-//		}catch(Exception e) {
-//			return null;
-//		} finally {
-//			try {
-//				closeDB(con, ps, rs);
-//			} catch (Exception e) {
-//				System.err.println(e.getMessage());
-//			}
-//		}
-//		return 戻り値;
-//	}
-
 
 //	DBアクセスメソッド
 	public static Connection accessDB() throws ClassNotFoundException,SQLException{
@@ -117,7 +83,7 @@ public class DAO {
 					loginFlg
 					);
 		} catch(Exception e) {
-			return null;
+			System.err.println(e.getMessage());
 		} finally {
 			try {
 				closeDB(con, ps, rs);
@@ -177,7 +143,7 @@ public class DAO {
 			}
 
 		} catch(Exception e) {
-			return null;
+			System.err.println(e.getMessage());
 		} finally {
 			try {
 				closeDB(con, ps, rs);
@@ -189,12 +155,54 @@ public class DAO {
 	}
 
 
+//	商品検索メソッド
+	public static List<ProductBean> searchProduct(String searchWord) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<ProductBean> addList = new ArrayList<ProductBean>();
+		List<ProductBean> returnList = new ArrayList<ProductBean>();
+		List<String> wordList = new ArrayList<String>();
+
+		try {
+//			引数の検索文字列を空白で区切ってListに格納する
+
+
+//			データ取得する
+			con =  accessDB();
+			String sql = "SELECT * FROM product WHERE productName LIKE %?%";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, 変数名);
+			rs = ps.executeQuery();
+
+//			取得データ格納用の変数を宣言する
+
+			while(rs.next()) {
+				rs.getString();
+			}
+
+//			インスタンス生成（Listに格納）
+
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				closeDB(con, ps, rs);
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+		}
+		return returnList;
+	}
+
+
 //	ログインメソッド
 	public static Integer login(String id,String pass) {
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		Integer i = -1;
+		int ret = -1;
 
 		try {
 //			引数のログインIDとパスワードに一致するユーザー情報を取得
@@ -212,22 +220,25 @@ public class DAO {
 				loginFlg = rs.getInt("loginFlg");
 
 //				ログインフラグが１（ログイン済）ならiに２（ログイン済であることを表す）を代入
-//				０（未ログイン）ならログインフラグを１に変更
+//				０（未ログイン）ならログインフラグを１に変更して、
+//				成功したらiに０（ログイン成功を表す）を代入
 				if(loginFlg == 1) {
 					i = 2;
 				}else {
 					String sql2 = "UPDATE user SET loginFlg = 1 WHERE loginId = ? AND password = ?";
 					ps.setString(1,id);
 					ps.setString(2, pass);
-					rs = ps.executeQuery();
-					i = 0;
+					ret = ps.executeUpdate();
+					if(ret != 0) {
+						i = 0;
+					}
 				}
 			}else {
 				i = 1;
 			}
 
 		}catch(Exception e) {
-			return null;
+			System.err.println(e.getMessage());
 		} finally {
 			try {
 				closeDB(con, ps, rs);
@@ -239,11 +250,13 @@ public class DAO {
 	}
 
 
+//	ログアウトメソッド
 	public static boolean logout(String id) {
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		boolean logout = false;
+		int ret = -1;
 
 		try {
 //		該当するユーザーのログインフラグを０（ログアウト状態）にする
@@ -251,13 +264,15 @@ public class DAO {
 			String sql = "UPDATE user SET loginFlg=0 WHERE loginId = ?";
 			ps = con.prepareStatement(sql);
 			ps.setString(1, id);
-			rs = ps.executeQuery();
+			ret = ps.executeUpdate();
 
 //		成功したら戻り値trueにする
-			logout = true;
+			if(ret != 0) {
+				logout = true;
+			}
 
 		}catch(Exception e) {
-			return logout;
+			System.err.println(e.getMessage());
 		} finally {
 			try {
 				closeDB(con, ps, rs);
@@ -269,11 +284,13 @@ public class DAO {
 	}
 
 
+//	新規登録メソッド
 	public static Integer signup(String id,String pass,String name,String post,String add,String telnum,String mailadd) {
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		Integer i = -1;
+		int ret = -1;
 
 		try {
 //			ログインIDが同一のユーザーがいるかどうか確認
@@ -297,12 +314,14 @@ public class DAO {
 				ps.setString(5, add);
 				ps.setString(6, telnum);
 				ps.setString(7, mailadd);
-				rs = ps.executeQuery();
-				i = 0;
+				ret = ps.executeUpdate();
+				if(ret != 0) {
+					i = 0;
+				}
 			}
 
 		}catch(Exception e) {
-			return null;
+			System.err.println(e.getMessage());
 		} finally {
 			try {
 				closeDB(con, ps, rs);
@@ -312,6 +331,161 @@ public class DAO {
 		}
 		return i;
 	}
+
+
+//	カート内情報取得メソッド
+	public static List<CartBean> showCart(String loginId) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<CartBean> list = new ArrayList<>();
+		int taxRate = 10;
+
+		try {
+//			ログインIDに紐づくユーザーのカート内の情報を取得する
+			con =  accessDB();
+			String sql = "SELECT * FROM cart INNER JOIN product ON cart.isbn = product.isbn WHERE cart.loginId = ?";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, loginId);
+			rs = ps.executeQuery();
+
+//			データ格納用の変数を用意、取得したデータを格納
+			String isbn;
+			String productName;
+			String authorName;
+			int price;
+			int taxPrice;
+			int quantity;
+
+			while(rs.next()) {
+				isbn = rs.getString("isbn");
+				productName = rs.getString("productName");
+				authorName = rs.getString("authorName");
+				price = rs.getInt("price");
+				taxPrice = price * (taxRate + 100) /100;
+				quantity = rs.getInt("quantity");
+
+//				インスタンス生成してListに追加
+				CartBean CartBean = new CartBean(
+						isbn,
+						productName,
+						authorName,
+						price,
+						taxPrice,
+						quantity
+						);
+
+				list.add(CartBean);
+			}
+
+		}catch(Exception e) {
+			System.err.println(e.getMessage());;
+		} finally {
+			try {
+				closeDB(con, ps, rs);
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+		}
+		return list;
+	}
+
+
+//	カートに商品を追加するメソッド
+	public static boolean addCart(String loginId,String isbn,int quantity) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		boolean add = false;
+		int ret = -1;
+
+		try {
+//			引数で指定されたログインID・isbnの組み合わせに一致する情報がカートテーブルにあるかをみる
+			con =  accessDB();
+			String sql = "SELECT * FROM cart WHERE loginId = ? AND isbn = ?";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, loginId);
+			ps.setString(2, isbn);
+			rs = ps.executeQuery();
+
+//			取得データがある場合（同一ユーザーが同一商品をすでにカートに入れている場合）はそのレコードの数量を変更し、
+//			ない場合は新しいレコードを追加する
+//			いずれも成功したら戻り値trueにする
+
+			if(rs.next()) {
+				int nowQuantity;
+				nowQuantity = rs.getInt("quantity");
+				quantity += nowQuantity;
+				String sql2 = "UPDATE cart SET quantity = ? WHERE loginId = ? AND isbn = ?";
+				ps = con.prepareStatement(sql2);
+				ps.setInt(1, quantity);
+				ps.setString(2, loginId);
+				ps.setString(3, isbn);
+				ret = ps.executeUpdate();
+				if(ret != 0) {
+					add = true;
+				}
+
+			}else {
+				String sql3 = "INSERT INTO cart VALUES(?,?,?)";
+				ps = con.prepareStatement(sql3);
+				ps.setString(1, loginId);
+				ps.setString(2, isbn);
+				ps.setInt(3, quantity);
+				ret = ps.executeUpdate();
+				if(ret != 0) {
+					add = true;
+				}
+			}
+
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				closeDB(con, ps, rs);
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+		}
+		return add;
+	}
+
+
+//	カート内の商品を全削除するメソッド
+	public static boolean deleteCart(String loginId) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		boolean delete = false;
+		int ret = -1;
+
+		try {
+//			引数で指定されたユーザーのカートの内容を全削除
+			con =  accessDB();
+			String sql = "DELETE FROM cart WHERE loginId = ?";
+			ps = con.prepareStatement(sql);
+			ps.setString(1,loginId);
+			ret = ps.executeUpdate();
+
+//			成功したら戻り値trueにする
+			if(ret != 0) {
+				delete = true;
+			}
+
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				closeDB(con, ps, rs);
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+		}
+		return delete;
+	}
+
+
+
 
 
 
